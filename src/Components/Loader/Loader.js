ac5cc90321ddef79715731477ab434382fbcd1b0
Alejandro Vivanco <manuelinviv@gmail.com>
2020-11-30 07:34:03 -0500

    cambio en el loader

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -3,7 +3,13 @@ import { LoaderC } from './Loaderestilos';
 
 const LoaderP = () => {
   return (
-    <div style={{ height: 800, padding: 200 }}>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 600,
+      }}>
       <LoaderC></LoaderC>
     </div>
   );
